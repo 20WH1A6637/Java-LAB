f356103495d6bb27564af3bc353648a2b37775d2
Thopuri Harshitha <93579922+20WH1A6637@users.noreply.github.com>
2022-03-29 12:01:01 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class Addition {
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+		int c = a + b;
+		System.out.println("The addition of two numbers is: " + c);
+	}
+}
